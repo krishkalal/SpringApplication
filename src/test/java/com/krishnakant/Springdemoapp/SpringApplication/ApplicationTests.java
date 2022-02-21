ca5225aa5db27ceb273ae20659b11cc95c6921d6
krishkalal <kalalkrishnakant@gmail.com>
2022-02-21 14:29:23 +0530

    first commit

---
+++ src/test/java/com/krishnakant/Springdemoapp/SpringApplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishnakant.Springdemoapp.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
